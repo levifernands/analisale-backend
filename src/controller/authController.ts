1bf93c779c2727888dbe3f698d36efc1cd70bed0
Levi Fernandes <levifernands31@gmail.com>
2023-11-22 16:28:54 -0300

    add authentication endpoint

---
+++ src/controller/authController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import User from "../models/User";
+import * as bcrypt from "bcrypt";
+import { generateToken } from "../authentication/jwtUtils";
+
+export const authenticate = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ where: { email } });
+    console.log("Usuário encontrado:", user);
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ message: "Credenciais inválidas" });
+    }
+
+    const token = generateToken({ email: user.email });
+    console.log("Token gerado:", token);
+
+    res.json({ token });
+  } catch (error) {
+    console.error("Erro ao autenticar usuário:", error);
+    res.status(500).json({ message: "Erro interno ao autenticar usuário" });
+  }
+};
--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import User from "../models/User";
+import * as bcrypt from "bcrypt";
 
 let users: { id: number; name: string; email: string }[] = [];
 
